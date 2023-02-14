f490123aef9e7a13be65ee2a7c2585932417d9c1
reeecheese <renalw2025@student.cis.edu.hk>
2023-02-14 14:59:15 +0800

    completed dawg

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,35 @@
 public class Dog {
+    private int age; private String name; private String color;
+
+    public Dog(){
+        age = 0; name = ""; color = "";
+    }
+    public Dog(int newage, String newname, String newcolor){
+        age = newage; name = newname; color = newcolor;
+    }
+    public int getAge(){
+        return age;}
+    public String getName(){
+        return name;}
+    public String getColor(){
+        return color;}
+    public void setAge(int newAge){
+        age = newAge;}
+    public void setName(String newName){
+        name = newName;}
+    public void setColor(String newColor){
+        color = newColor;}
+
+    private void addOneAge(){
+        age++;}
+
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+    public String toString(){
+        return "The dog is named "+ name + " and is " + age + " years old. This dog is " + color + ".";
+    }
+
+
 }
--- src/DogMaker.java
+++ src/DogMaker.java
@@ -1,6 +1,12 @@
 public class DogMaker {
-  
+
   public static void main(String[] args) {
+    new Dog(2, "jill", "black and tan");
+    Dog.setName("Jessie");
+    System.out.println(Dog.getAge());
+    int age = Dog.birthday();
+    System.out.println(age);
+    System.out.println(Dog.toString());
     
     
   }
